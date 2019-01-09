fbf20311df9a2969b0e0ba57cc597141bf10f60a
David Huveau <d.huveau@gmail.com>
2019-01-09 10:30:07 +0100

    test routes

--- index.js
+++ index.js
@@ -2,9 +2,44 @@ const express = require("express");
 const app = express();
 const models = require("./models");
 
-app.get("/", (req, res) => {
-  res.json(process.env);
-  res.end;
+app.post("/users", (req, res) => {
+  const newUser = new models.users({
+    name: "toto",
+    age: 21
+  });
+  newUser.save();
+  // res.json(process.env);
+  res.send("insert ok");
+});
+
+app.get("/users", (req, res) => {
+  // models.users.findById(1).then(data => {
+  //   res.json(data);
+  // });
+  // models.users.find({ where: { id: 1 } }).then(data => {
+  //   res.json(data);
+  // });
+  models.users
+    .find({
+      where: { id: 1 },
+      include: [
+        {
+          model: models.orders
+        }
+      ]
+    })
+    .then(data => {
+      res.json(data);
+    });
+});
+
+app.post("/orders", (req, res) => {
+  const newOrder = new models.orders({
+    ref: "1234",
+    userId: 1
+  });
+  newOrder.save();
+  res.send("insert ok");
 });
 
 models.sequelize.sync().then(() => {
